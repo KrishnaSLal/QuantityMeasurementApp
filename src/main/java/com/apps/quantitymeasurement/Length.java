fa2e18393f6b2e8d69fa9f94a930ec0c3875030b
KrishnaSLal <krishnaslal0204@gmail.com>
2026-02-23 23:02:07 +0530

    [KrishnaSLal]: Added UC5 Unit to Unit Conversion

--- src/main/java/com/apps/quantitymeasurement/Length.java
+++ src/main/java/com/apps/quantitymeasurement/Length.java
@@ -23,7 +23,10 @@ public class Length {
 	}
 	
 	// constructor to initialize length and unit
-	public Length(double value, LengthUnit unit) {		
+	public Length(double value, LengthUnit unit) {	
+		if(unit == null) throw new IllegalArgumentException("Enter a valid length unit");
+		if(!Double.isFinite(value)) throw new IllegalArgumentException("Enter a valid double value for conversion");
+
 		this.value = value;
 		this.unit = unit;
 	}
@@ -34,11 +37,29 @@ public class Length {
 	}
 	
 	// compare two length object
+	private static final double EPSILON = 0.0001;
+
+	private double normalizedBaseValue() {
+	    return Math.round(convertToBaseUnit() * 10000.0) / 10000.0;
+	}
+
 	public boolean compare(Length thatLength) {
-		double currentBaseLength = this.convertToBaseUnit();
-		double thatBaseLength = thatLength.convertToBaseUnit();
+	    if (thatLength == null) return false;
+
+	    return Double.compare(
+	            this.normalizedBaseValue(),
+	            thatLength.normalizedBaseValue()
+	    ) == 0;
+	}
+	
+	// convert to other unit
+	public Length convertTo(LengthUnit targetUnit) {
+		if(targetUnit == null) throw new IllegalArgumentException("Enter a valid unit for conversion");
 		
-		return Double.compare(currentBaseLength, thatBaseLength) == 0;
+		double toBaseUnit = this.convertToBaseUnit();
+		double resultantValue = toBaseUnit / targetUnit.getConversionFactor();
+		
+		return new Length(resultantValue, targetUnit);
 	}
 	
     @Override
@@ -49,6 +70,11 @@ public class Length {
 
         Length thatLength = (Length) o;
         return compare(thatLength);
+    }
+    
+    @Override
+    public String toString() {
+    	return this.value + "" + unit;
     }
     
     public static void main(String[] args) {
@@ -63,5 +89,7 @@ public class Length {
         Length length5 = new Length(100.0, LengthUnit.CENTIMETERS);
         Length length6 = new Length(39.3701, LengthUnit.INCHES);
         System.out.println("Are lengths equal : " + length5.equals(length6));
+        
+        System.out.println("36 Inches to equals to : " + length4.convertTo(LengthUnit.YARDS));
     }
 }
